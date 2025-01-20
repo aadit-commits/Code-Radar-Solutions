35e61fff5aa4cf4856d482f0ccff734773860d35
Aadit <mail2aaditsarhadi@gmail.com>
2025-01-20 10:16:46 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -6,6 +6,17 @@ int main() {
     if (g=='A'){
         printf("Excellent");
     }
-    
+    else if (g=='B'){
+        printf("Good");
+    }
+    else if (g=='C'){
+        printf("Average");
+    }
+    else if (g=='D'){
+        printf("Below Average");
+    }
+    else {
+        printf("Fail");
+    }
     return 0;
 }
